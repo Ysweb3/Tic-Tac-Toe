d4a9e08fae06a54b0df51c2dd760845ba4da9849
Ysweb3 <yssh88972@gmail.com>
2024-09-01 00:00:39 +0530

    Add GameBoard create function

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-function createBoard() {
-    let defaultBoard = [['-','-','-'],
-                        ['-','-','-'],
-                        ['-','-','-']]
-    const getBoard = () => defaultBoard;
-  
-    return { getBoard };
+
+let defaultBoard = [['-','-','-'],
+                    ['-','-','-'],
+                    ['-','-','-']]
+
+function CreateBoard(newGameBoard) {
+    return [...newGameBoard];
   }
   
-
 const CreatePlayer = function(name,score,mark) {
     this.name = name,
     this.score = score;
@@ -16,10 +15,28 @@ const CreatePlayer = function(name,score,mark) {
 
 const player1 = new CreatePlayer;
 const player2 = new CreatePlayer;
-const newGameBoard = createBoard();
 
-function CreateGame(){
+function InitializePlayers(){
+    player1.name = "Player 1";//take names from user 
+    player1.score = 0;
+    player1.mark = "O";
+    
+    player2.name = "Player 2"
+    player2.score = 0;
+    player1.mark = "X";
+}
+
+function StartGame(){
+    let win = false;
+    let GameBoard = CreateBoard(defaultBoard);
+    InitializePlayers();
+    console.log("Welcome")
+    console.log(GameBoard);
+    while (win == false) {
+        win = true
+        
+    }
 
 }
 
-console.log(newGameBoard.getBoard())
+StartGame();
