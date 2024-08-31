2c164ba210d2256bd69f12f7280e2c2fc2a5b83f
Ysweb3 <yssh88972@gmail.com>
2024-08-31 23:26:48 +0530

    Fix Create Objects for Game

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-const gameBoard = {
-    gameBoard : ['wdawd','awdawd']
+function createBoard() {
+    let defaultBoard = [['-','-','-'],
+                        ['-','-','-'],
+                        ['-','-','-']]
+    const getBoard = () => defaultBoard;
+  
+    return { getBoard };
+  }
+  
+
+const CreatePlayer = function(name,score,mark) {
+    this.name = name,
+    this.score = score;
+    this.mark = mark;
 }
 
-createGame = new gameBoard;
-console.log(gameBoard.gameBoard);
+const player1 = new CreatePlayer;
+const player2 = new CreatePlayer;
+const newGameBoard = createBoard();
+
+function CreateGame(){
+
+}
+
+console.log(newGameBoard.getBoard())
