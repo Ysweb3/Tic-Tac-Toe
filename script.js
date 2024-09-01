9bca50dd7140f5322ee096fc65e99850af92e1b4
Ysweb3 <yssh88972@gmail.com>
2024-09-02 00:28:07 +0530

    Add game logic

--- script.js
+++ script.js
@@ -23,17 +23,39 @@ function InitializePlayers(){
     
     player2.name = "Player 2"
     player2.score = 0;
-    player1.mark = "X";
+    player2.mark = "X";
 }
 
 function StartGame(){
     let win = false;
+    let runGame = null;
+    let playerTurn = 1;
+    let arrayIndex1 = null;
+    let arrayIndex2 = null;
     let GameBoard = CreateBoard(defaultBoard);
     InitializePlayers();
+    let p1mark = player1.mark;
+    let p2mark = player2.mark;
     console.log("Welcome")
     console.log(GameBoard);
-    while (win == false) {
-        win = true
+    runGame = prompt("Please enter true to start the game.");
+    while (runGame == "true" && win == false){
+        console.log(GameBoard);
+        
+        if (playerTurn == 1){
+            arrayIndex1 = prompt("Enter the first array index");
+            arrayIndex2 = prompt("Enter the secound array index");
+            GameBoard[arrayIndex1][arrayIndex2] = p1mark;
+            playerTurn = 2;
+        }
+        else if (playerTurn ==2){
+            arrayIndex1 = prompt("Enter the first array index");
+            arrayIndex2 = prompt("Enter the secound array index");
+            GameBoard[arrayIndex1][arrayIndex2] = p2mark;
+            
+            playerTurn = 1;
+        }
+
         
     }
 
